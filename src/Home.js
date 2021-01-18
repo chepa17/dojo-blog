ad372d6b8a43eded50668918aecad33a503a3cb0
Chepkyi Mykola <chepkiyn@gmail.com>
2021-01-18 18:54:46 +0200

    add json server

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,41 @@
+import { useState, useEffect } from 'react';
+import BlogList from './BlogList';
+
 const Home = () => {
+    const [blogs, setBlogs] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-const handleClick = (e) => {
-    console.log('hello, ninjas', e)
-}
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    };
 
-const handleClickAgain = (name, e) => {
-    console.log('hello ' + name, e.target)
-}
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                if (!res.ok) {
+                    throw Error('could not fetch the data for that resource');
+                }
+                return res.json();
+            })
+            .then((data) => {
+                setBlogs(data)
+                setIsLoading(false);
+                setError(null);
+            })
+            .catch(err => {
+                setError(err.message);
+                setIsLoading(false);
+            })
+    }, []);
 
-return ( 
-    <div className="home">
-        <h2>Homepage</h2>
-        <button onClick={handleClick}>Click me</button>
-        <button onClick={(e) =>handleClickAgain('mario', e)}>Click me again</button>
-    </div>
+    return ( 
+        <div className='home'>
+            { error && <div>{error}</div>}
+            { isLoading && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title='All Blogs!' handleDelete={handleDelete} />}
+        </div>
     );
 }
  
