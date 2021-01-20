7a87f4bb5c3e965adc5847d9e470be8b0271af7f
Chepkyi Mykola <chepkiyn@gmail.com>
2021-01-20 14:20:02 +0200

    add rotes and blog details page

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,7 +6,10 @@ const useFetch = (url) => {
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        fetch(url)
+        const abortCont = new AbortController();
+
+        setTimeout(() => {
+            fetch(url, { signal: abortCont.signal })
             .then(res => {
                 if (!res.ok) {
                     throw Error('could not fetch the data for that resource');
@@ -19,9 +22,17 @@ const useFetch = (url) => {
                 setError(null);
             })
             .catch(err => {
-                setError(err.message);
-                setIsLoading(false);
+                if (err.name === 'AbortError') {
+                    console.log('fetch aborted')
+                } else {
+                    setError(err.message);
+                    setIsLoading(false);
+                }
             })
+        }, 1000)
+        
+
+        return () => abortCont.abort();
     }, [url]);
 
     return { data, isLoading, error}
